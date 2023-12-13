0df80ceb77abb9f8c34be4fef5adaa68b5c4b6c1
Thongboss <huuthong251099@gmail.com>
2023-12-13 15:20:04 +0700

    create order form

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 import { HomeComponent } from './app/home/home.component';
+import { OrderComponent } from './app/order/order.component';
 
-const bootstrap = () => bootstrapApplication( HomeComponent, config);
+const bootstrap = () => bootstrapApplication( OrderComponent, config);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { HomeComponent } from './app/home/home.component';
+import { OrderComponent } from './app/order/order.component';
 // import { AppComponent } from './app/app.component';
 
-bootstrapApplication( HomeComponent, appConfig)
+bootstrapApplication( OrderComponent, appConfig)
   .catch((err) => console.error(err));
